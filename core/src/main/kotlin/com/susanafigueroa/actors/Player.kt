1cc52da00db66f93b83575d71c4105ef7e5fab33
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-01-23 20:58:47 +0100

    override act method in Player class to enable player movement

--- core/src/main/kotlin/com/susanafigueroa/actors/Player.kt
+++ core/src/main/kotlin/com/susanafigueroa/actors/Player.kt
@@ -1,11 +1,13 @@
 package com.susanafigueroa.actors
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Actor
+import com.susanafigueroa.utils.GameInfo
 
 class Player : Actor() {
 
@@ -15,6 +17,7 @@ class Player : Actor() {
         const val INITIAL_POSITION_X_PLAYER: Float = 80f
         const val INITIAL_POSITION_Y_PLAYER: Float = 80f
         const val PATH_TEXTURE_PLAYER: String = "player/player.png"
+        const val SPEED: Float = 100f
     }
 
     private val texturePlayer: Texture
@@ -33,6 +36,47 @@ class Player : Actor() {
 
         // adjudico podición inicial al rectanglePlayer
         rectanglePlayer = Rectangle(positionPlayer.x, positionPlayer.y, WIDTH_PLAYER, HEIGHT_PLAYER)
+    }
+
+    override fun act(delta: Float) {
+        super.act(delta)
+
+        // Procesar entradas del teclado para mover al jugador
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) { // Mover arriba
+            moveBy(0f, SPEED * delta)
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) { // Mover abajo
+            moveBy(0f, -SPEED * delta)
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) { // Mover izquierda
+            moveBy(-SPEED * delta, 0f)
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) { // Mover derecha
+            moveBy(SPEED * delta, 0f)
+        }
+
+        val valueTouchX = Gdx.input.getX()
+        val valueTouchY = Gdx.input.getY()
+
+        if (Gdx.input.isTouched) {
+            if (valueTouchY > GameInfo.HEIGHT/2) { // Mover arriba
+                moveBy(0f, -SPEED * delta)
+            }
+            if (valueTouchY < GameInfo.HEIGHT/2) { // Mover abajo
+                moveBy(0f, SPEED * delta)
+            }
+            if (valueTouchX < GameInfo.WIDTH/2) { // Mover izquierda
+                moveBy(-SPEED * delta, 0f)
+            }
+            if (valueTouchX > GameInfo.WIDTH/2) { // Mover derecha
+                moveBy(SPEED * delta, 0f)
+            }
+
+
+        }
+
+        // Actualizar la posición del rectángulo
+        rectanglePlayer.setPosition(x, y)
     }
 
     override fun draw(batch: Batch, parentAlpha: Float) {
--- core/src/main/kotlin/com/susanafigueroa/utils/GameInfo.kt
+++ core/src/main/kotlin/com/susanafigueroa/utils/GameInfo.kt
@@ -6,10 +6,6 @@ class GameInfo {
         // tamaño lógico del área visible del mundo
         const val WIDTH = 1280
         const val HEIGHT = 720
-
-        // constantes player
-        val TEXTURE_PATH_PLAYER: String = "player/player.png"
-
     }
 
 }
